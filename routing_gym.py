0624f9c18df141fd2ab7bbbf5bf21a31e4c1494d
bhtu4897 <55126915+bhtu4897@users.noreply.github.com>
2024-04-24 15:23:03 +0800

    Update routing_gym.py

--- routing_gym.py
+++ routing_gym.py
@@ -7,38 +7,42 @@ from copy import deepcopy
 
 #這個環境是一個多智能體路徑規劃問題的仿真器，用於研究多智能體如何在一個網格地圖上找到最佳路徑以達到各自的目標。
 
-
+# 狀態類別
 class State(object):
     def __init__(self, starts, goals, wsize=20):
-        self.agents = deepcopy(starts)
-        self.goals = deepcopy(goals)
-        self.paths = [[pt] for pt in starts]
-        self.num_agents = len(goals)
-        self.wsize = wsize
-        self.move_list = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        self.agents = deepcopy(starts)  # 深度複製起點位置
+        self.goals = deepcopy(goals)  # 深度複製目標位置
+        self.paths = [[pt] for pt in starts]  # 路徑列表，初始為起點位置
+        self.num_agents = len(goals)  # 智能體的數量
+        self.wsize = wsize  # 地圖大小
+        self.move_list = ((-1, 0), (0, 1), (1, 0), (0, -1))  # 移動方向列表
 
-
+    # 獲取觀察值
     def getObservation(self, ag_id):
         obs = np.stack((self.get_obstacle(ag_id), self.get_agents_pos(ag_id), self.get_neighbors_goal(ag_id),
                         self.get_agent_goal(ag_id)))
         return obs
-
+        
+    # 獲取智能體位置
     def getPos(self, ag_id):
         return self.agents[ag_id - 1]
 
-
+    # 獲取智能體路徑
     def getPath(self, ag_id):
         return self.paths[ag_id - 1]
 
+    # 獲取目標位置
     def getGoal(self, ag_id):
         return self.goals[ag_id - 1]
 
+    # 獲取到目標的距離
     def getDistance(self, ag_id):
         px, py = self.agents[ag_id - 1]
         gx, gy = self.goals[ag_id - 1]
         distance = (gx - px, gy - py)
         return distance
 
+    # 獲取障礙物
     def getObset(self):
         obs_set = set()
         for path in self.paths:
@@ -46,11 +50,12 @@ class State(object):
 
         return obs_set
 
+     # 執行動作
     def act(self, ag_id, action):
 
         """
         execute the action and return the status of the agent
-
+        執行動作並返回代理狀態
         """
         index = ag_id - 1
         dxo, dyo = self.getDistance(ag_id)
@@ -59,14 +64,14 @@ class State(object):
         ay = self.agents[index][1] + ag_move[1]
         next_pos = (ax, ay)
         reward = 0.0
-        if next_pos == self.getGoal(ag_id):  # reach the goal
+        if next_pos == self.getGoal(ag_id):  # reach the goal 到達目標
             self.agents[index] = next_pos
             reward = 20
-        elif not (0 <= ax < 20 and 0 <= ay < 20):  # out of bound
+        elif not (0 <= ax < 20 and 0 <= ay < 20):  # out of bound 超出邊界
             reward = -2
-        elif next_pos in self.getObset():  # self collision
+        elif next_pos in self.getObset():  # self collision 自己碰撞
             reward = -2
-        elif next_pos in self.goals:  # reach others' goals
+        elif next_pos in self.goals:  # reach others' goals 到達其他智能體的目標
             reward = -2
         else:
             self.agents[index] = next_pos
@@ -81,11 +86,13 @@ class State(object):
         return reward
 
 
+     # 確定是否完成 到目標
     def done(self):
         return self.agents == self.goals
 
+    # 獲取障礙物
     def get_obstacle(self, id):
-        gmap = np.ones((self.wsize, self.wsize))  # 初始化全為1的map
+        gmap = np.ones((self.wsize, self.wsize))  # 初始化全為1的map 1為可走 0為障礙物
 
         for ag_id in range(1, self.num_agents + 1):
             for position in self.paths[ag_id - 1]:
@@ -93,7 +100,7 @@ class State(object):
                 if 0 <= x < self.wsize and 0 <= y < self.wsize:
                     gmap[x, y] = 0  # 已被走過的路設為0 (障礙物)
 
-        # 获取代理为中心的9x9地图ㄖ
+        # 獲取代理為中心的9x9地圖
         agent_pos = self.getPos(id)
         central_map_size = 9
         central_map = np.zeros((central_map_size,central_map_size))
@@ -114,6 +121,7 @@ class State(object):
         # print(central_map)
         return central_map.astype(np.float32)
 
+    # 獲取智能體位置
     def get_agents_pos(self, id):
         central_map_size = 9
         # 以agent為中心的9X9觀察空間
@@ -130,6 +138,7 @@ class State(object):
 
         return central_map.astype(np.float32)
 
+    # 獲取鄰近的終點
     def get_neighbors_goal(self, id):
         central_map_size = 9
         # 以agent為中心的9X9觀察空間
@@ -147,6 +156,7 @@ class State(object):
 
         return central_map.astype(np.float32)
 
+    # 獲取智能體終點
     def get_agent_goal(self, id):
         central_map_size = 9
         # 以agent為中心的9X9觀察空間
@@ -162,7 +172,7 @@ class State(object):
 
         return central_map.astype(np.float32)
 
-
+# 路徑規劃環境
 class RoutingEnv(gym.Env):
     def __init__(self, starts, goals, wsize=20):
         self.wsize = 20
